3ee3138285faf31198d0d1a46d72a73eb88494c0
gameblabla <gameblabla@protonmail.com>
2021-12-31 02:54:07 +0100

    Further improvements to Dreamcast version.

--- src/dc/sdl12.cpp
+++ src/dc/sdl12.cpp
@@ -34,19 +34,15 @@ r8::io::Loader loader;
 
 r8::input::InputManager input;
 r8::gfx::ColorTable colorTable;
-int16_t* audioBuffer;
 
 static unsigned start = timer_ms_gettime64();
-
-#define _32BPP_PAL 1
-
 
 #ifdef _8BPP
 
 #ifdef _32BPP_PAL
 #define PACK_ARGB8888(a,r,g,b) ( ((a & 0xFF) << 24) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF) )
 #else
-#define RGB565(r, g, b) (unsigned short)((r << 11) + (g << 5) + b)
+#define PACK_RGB565(r,g,b) ( ((r >> 3) & 0x1f) << 11 | ((g >> 2) & 0x3f) << 5 | (b >> 3) & 0x1f )
 #endif
 
 uint32_t pal_rgb[16];
@@ -79,9 +75,8 @@ static void Set_Pal_col(uint8_t r, uint8_t g, uint8_t b, uint16_t entry)
 	#ifdef _32BPP_PAL
 	pal_rgb[entry] = PACK_ARGB8888(255,r,g,b);
 	#else
-	pal_rgb[entry] = RGB565(r,g,b);
+	pal_rgb[entry] = PACK_RGB565(r,g,b);
 	#endif
-	
 	for(i=0;i<16;i++)
 	{
 		pvr_set_pal_entry(entry+(16*i), pal_rgb[entry]);
@@ -114,47 +109,46 @@ struct ColorMapper
 
 uint32_t Platform::getTicks() { return timer_ms_gettime64()-start; }
 
-void deinit()
-{
-	delete[] audioBuffer;
-	//TODO: release all structures bound to Lua etc
-}
-
 //**/
 
-
-pvr_ptr_t back_tex;
 pvr_ptr_t front_tex;
 void back_init()
 {
 #ifdef _8BPP
-    back_tex = pvr_mem_malloc(128*128);
     front_tex = pvr_mem_malloc(128*128);
 #else
-    back_tex = pvr_mem_malloc(128*128*2);
     front_tex = pvr_mem_malloc(128*128*2);
 #endif
 }
 
+/* Linear/iterative twiddling algorithm from Marcus' tatest */
+#define TWIDTAB(x) ( (x&1)|((x&2)<<1)|((x&4)<<2)|((x&8)<<3)|((x&16)<<4)| \
+	((x&32)<<5)|((x&64)<<6)|((x&128)<<7)|((x&256)<<8)|((x&512)<<9) )
+#define TWIDOUT(x, y) ( TWIDTAB((y)) | (TWIDTAB((x)) << 1) )
+#define MIN(a, b) ( (a)<(b)? (a):(b) )
+
 /* draw background */
 void draw_back()
 {
-#ifdef _8BPP
-	pvr_txr_load_ex(mem, front_tex, 128, 128, PVR_TXRLOAD_8BPP);
-#else
-    pvr_txr_load_dma(mem, front_tex, 128*128*2, 1, NULL, NULL);
-#endif
-    
     pvr_poly_cxt_t cxt;
     pvr_poly_hdr_t hdr;
     pvr_vertex_t vert;
-   
-    //PVR_TXRFMT_PAL8BPP
-    #ifdef _8BPP
-    pvr_poly_cxt_txr(&cxt, PVR_LIST_OP_POLY, PVR_TXRFMT_PAL8BPP| PVR_TXRFMT_8BPP_PAL(0)|PVR_TXRFMT_TWIDDLED|PVR_TXRFMT_NOSTRIDE|PVR_TXRFMT_VQ_DISABLE, 128, 128, front_tex, PVR_FILTER_BILINEAR);
-    #else
+#ifdef _8BPP
+	uint_fast8_t x, y;
+	uint16 *vtex = (uint16*)front_tex;
+	for (y=0; y<128; y += 2)
+	{
+		for (x=0; x<128; x++)
+		{
+			vtex[TWIDOUT((y&127)/2, x&127) + (x/128 + y/128)*128*128/2] = mem[y*128+x] | (mem[(y+1)*128+x]<<8);
+		}
+	}
+	pvr_poly_cxt_txr(&cxt, PVR_LIST_OP_POLY, PVR_TXRFMT_PAL8BPP| PVR_TXRFMT_8BPP_PAL(0)|PVR_TXRFMT_TWIDDLED|PVR_TXRFMT_NOSTRIDE|PVR_TXRFMT_VQ_DISABLE, 128, 128, front_tex, PVR_FILTER_NONE);
+#else
+	pvr_txr_load_dma(mem, front_tex, 128*128*2, 1, NULL, NULL);
 	pvr_poly_cxt_txr(&cxt, PVR_LIST_OP_POLY, PVR_TXRFMT_RGB565|PVR_TXRFMT_NONTWIDDLED|PVR_TXRFMT_NOSTRIDE|PVR_TXRFMT_VQ_DISABLE, 128, 128, front_tex, PVR_FILTER_BILINEAR);
-	#endif
+#endif
+
     pvr_poly_compile(&hdr, &cxt);
     pvr_prim(&hdr, sizeof(hdr));
 
@@ -162,28 +156,28 @@ void draw_back()
     vert.oargb = 0;
     vert.flags = PVR_CMD_VERTEX;
     
-    vert.x = 1;
+    vert.x = 80;
     vert.y = 1;
     vert.z = 1;
     vert.u = 0.0;
     vert.v = 0.0;
     pvr_prim(&vert, sizeof(vert));
     
-    vert.x = 640;
+    vert.x = 560;
     vert.y = 1;
     vert.z = 1;
     vert.u = 1.0;
     vert.v = 0.0;
     pvr_prim(&vert, sizeof(vert));
     
-    vert.x = 1;
+    vert.x = 80;
     vert.y = 480;
     vert.z = 1;
     vert.u = 0.0;
     vert.v = 1.0;
     pvr_prim(&vert, sizeof(vert));
     
-    vert.x = 640;
+    vert.x = 560;
     vert.y = 480;
     vert.z = 1;
     vert.u = 1.0;
@@ -211,8 +205,17 @@ void draw_frame()
 
 void pvr_setup()
 {
+	pvr_init_params_t params = {
+		/* Enable opaque, translucent polygons and punch thru with size 16 */
+		{ PVR_BINSIZE_16, PVR_BINSIZE_0, PVR_BINSIZE_16, PVR_BINSIZE_0, PVR_BINSIZE_16 },
+		
+		/* Vertex buffer size 512K */
+		512*1024
+	};
+
     /* init kos  */
-    pvr_init_defaults();
+    vid_set_mode(DM_640x480, PM_RGB565);
+	pvr_init(&params);
     pvr_dma_init();
    
     /* init background */
@@ -244,8 +247,6 @@ void flip_screen()
 	
 	draw_frame();
 
-	++frameCounter;
-
 	/* Of course this assumes that the actual screen refresh rate is 60 or close to it.
 	 * Most games are 30 FPS though. I'm not aware of any 60 fps games aside from one
 	 * which only works on Picolove. - Gameblabla */
@@ -271,7 +272,6 @@ bool load_game(char* rom_name)
 	input.reset();
 	machine.setflip(flip_screen);
 	machine.sound().init();
-	frameCounter = 0;
 	
 	if (strstr(rom_name, ".PNG") || strstr(rom_name, ".png"))
 	{
@@ -308,7 +308,7 @@ bool load_game(char* rom_name)
 	return true;
 }
 
-
+#ifdef SDL_SOUND_DC
 void audio_callback(void* data, uint8_t* cbuffer, int length)
 {
 	retro8::sfx::APU* apu = static_cast<retro8::sfx::APU*>(data);
@@ -316,14 +316,14 @@ void audio_callback(void* data, uint8_t* cbuffer, int length)
 	apu->renderSounds(buffer, length / sizeof(int16_t));
 	return;
 }
-
-
-static int16_t sound_buffer[SAMPLE_RATE * 4];
+#else
+static int16_t sound_buffer[SAMPLE_RATE * 2];
 static void *sound_callback(snd_stream_hnd_t hnd, int len, int *actual)
 {
-	machine.sound().renderSounds(sound_buffer, 2048);
+	machine.sound().renderSounds(sound_buffer, len / sizeof(int16_t));
 	return (int16_t *)(sound_buffer);
 }
+#endif
 
 uint_fast8_t retro_run()
 {
@@ -414,6 +414,7 @@ int main(int argc, char* argv[])
 #endif
 
 	/* TODO : Get rid of SDL dependency for Sound */
+	#ifdef SDL_SOUND_DC
 	SDL_Init(SDL_INIT_AUDIO);
 	SDL_AudioSpec wantSpec, spec;
 	wantSpec.freq = 44100;
@@ -425,6 +426,7 @@ int main(int argc, char* argv[])
 
 	SDL_OpenAudio(&wantSpec, &spec);
 	SDL_PauseAudio(0);
+	#endif
 
 	printf("Initializing audio buffer of %zu bytes\n", sizeof(int16_t) * SAMPLE_RATE * 2);
 
@@ -445,19 +447,21 @@ int main(int argc, char* argv[])
 		printf("Could not load game '%s'!\n", argv[1]);
 		return 0;
 	}
-
-    /*snd_stream_init();
-    snd_dc = snd_stream_alloc(sound_callback, SAMPLE_RATE * 4);
-	snd_stream_start(snd_dc,44100, 0);*/
+	#ifndef SDL_SOUND_DC
+    snd_stream_init();
+    snd_dc = snd_stream_alloc(sound_callback, SAMPLE_RATE * 2);
+	snd_stream_start(snd_dc,44100, 0);
+	#endif
 		
 	while(while_res)
 	{
 		while_res = retro_run();
 	}
-	
+	#ifdef SDL_SOUND_DC
 	SDL_PauseAudio(1);
-	deinit();
 	SDL_Quit();
+	#endif
+	
 		
 	return 0;
 }
